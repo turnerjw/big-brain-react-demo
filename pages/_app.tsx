8e37e90b3236d247ce77db7fccfc09ccd734d456
Justin Turner <turnerjw@rogers.com>
2021-06-27 18:43:32 -0400

    Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="container mx-auto px-10">
+      <Link href="/">
+        <a>
+          <h1 className="text-7xl font-bold mt-10">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+              Big Brain React
+            </span>
+          </h1>
+        </a>
+      </Link>
+      <div className="p-10">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,36 +2,29 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-10">
-      <h1 className="text-7xl font-bold mt-10">
-        <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
-          Big Brain React
-        </span>
-      </h1>
-      <div className="mt-10 grid grid-cols-2 gap-5">
-        <Link href="/a">
-          <a>
-            <div className="rounded-lg p-5 bg-gray-50 transition-shadow hover:shadow-xl">
-              <h3 className="text-4xl font-bold">Component A</h3>
-              <p className="text-xl text-gray-700 mt-3">
-                Displays Pokémon in rows that scroll vertically. Each pokémon
-                can be clicked to indicate that it has been caught.
-              </p>
-            </div>
-          </a>
-        </Link>
-        <Link href="/b">
-          <a>
-            <div className="group rounded-lg p-5 bg-gray-50 transition-shadow hover:shadow-xl">
-              <h3 className="text-4xl font-bold">Component B</h3>
-              <p className="text-xl text-gray-700 mt-3">
-                Displays Pokémon in cards that scroll horizontally. Each pokémon
-                can be clicked to indicate that it has been caught.
-              </p>
-            </div>
-          </a>
-        </Link>
-      </div>
+    <div className="grid grid-cols-2 gap-5">
+      <Link href="/a">
+        <a>
+          <div className="rounded-lg p-5 bg-gray-50 transition-shadow hover:shadow-2xl">
+            <h3 className="text-4xl font-bold">Component A</h3>
+            <p className="text-xl text-gray-700 mt-3">
+              Displays Pokémon in rows that scroll vertically. Each pokémon can
+              be clicked to indicate that it has been caught.
+            </p>
+          </div>
+        </a>
+      </Link>
+      <Link href="/b">
+        <a>
+          <div className="group rounded-lg p-5 bg-gray-50 transition-shadow hover:shadow-xl">
+            <h3 className="text-4xl font-bold">Component B</h3>
+            <p className="text-xl text-gray-700 mt-3">
+              Displays Pokémon in cards that scroll horizontally. Each pokémon
+              can be clicked to indicate that it has been caught.
+            </p>
+          </div>
+        </a>
+      </Link>
     </div>
   );
 }
