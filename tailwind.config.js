669267747f7c1502082ceac2102df458188cac10
Justin Turner <turnerjw@rogers.com>
2021-06-27 22:58:35 -0400

    Fix styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    options: {
+      safelist: [
+        "from-green-200",
+        "to-green-300",
+        "from-red-200",
+        "to-red-300",
+        "from-blue-200",
+        "to-blue-300",
+        "from-yellow-200",
+        "to-yellow-300",
+      ],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
